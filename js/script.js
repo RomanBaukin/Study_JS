bb53160dd86210de19a99f423dd10cd0cba39a44
Roman Baukin <rbaukin1984@gmail.com>
2020-11-20 14:59:37 +0400

    Урок №2 обязательное задание

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-let money,
-    income,
-    addExpenses, 
-    deposit,
-    mission, 
-    period;
+let money = 50000;
+let income = '10000 фриланс';
+let addExpenses = 'Интернет, Такси, Коммуналка'; 
+let deposit = true;
+let mission = 1000000; 
+let period = 12;
 
-alert('Привет, Мир!');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log(addExpenses);
 
-console.log('Я учу JS');
+let budgetDay = money / 30;
+console.log(`Дневной бюджет равен ${budgetDay} р.`);
