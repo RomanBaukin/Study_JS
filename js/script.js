6a997144fb58edbb32a11481c0705baf0c784c1d
Roman Baukin <rbaukin1984@gmail.com>
2020-11-20 16:06:37 +0400

    Урок №2 усложненное задание

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-let money = 50000;
-let income = '10000 фриланс';
-let addExpenses = 'Интернет, Такси, Коммуналка'; 
-let deposit = true;
-let mission = 1000000; 
-let period = 12;
+let num = 266219, lastNumber, result = 1;
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-console.log(addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-addExpenses = addExpenses.toLowerCase().split(', ');
-console.log(addExpenses);
+while (num) {
+  lastNumber = num % 10;
+  result *= lastNumber;
+  num = (num - lastNumber) / 10;
+}
 
-let budgetDay = money / 30;
-console.log(`Дневной бюджет равен ${budgetDay} р.`);
+result = String(result**3);
+console.log(result.substring(0, 2));
