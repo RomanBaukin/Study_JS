83ae1d3f2ef675dafce6473217406543b986bc9e
Roman Baukin <rbaukin1984@gmail.com>
2020-11-23 16:40:06 +0400

    Урок №3 усложненное задание

--- js/script.js
+++ js/script.js
@@ -1,40 +1,33 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход'),
-  income = '10000 фриланс',
-  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-  deposit = confirm('Есть ли у вас депозит в банке?'),
-  expenses1 = prompt('Введите обязательную статью расходов'),
-  amount1 = +prompt('Во сколько это обойдётся?'),
-  expenses2 = prompt('Введите обязательную статью расходов'),
-  amount2 = +prompt('Во сколько это обойдётся?'),
-  mission = 1000000,
-  period = 12,
-  budgetMonth = money - amount1 - amount2,
-  budgetDay = Math.floor(budgetMonth / 30),
-  monthsToTarget = Math.ceil(mission / budgetMonth),
-  incomeLevel;
+let lang = prompt('Введите "ru" или "en"'),
+  arrDayOfTheWeek = [
+    ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  ],
+  dayOfTheWeekIfElse,
+  dayOfTheWeekSwitchCase,
+  dayOfTheWeekArray = lang === 'ru' ? arrDayOfTheWeek[0] : arrDayOfTheWeek[1],
+  namePerson = prompt('Введите имя или статус'),
+  statusPerson;
 
-addExpenses = addExpenses.toLowerCase().split(', ');
-
-if (budgetDay > 1200) {
-  incomeLevel = 'У вас высокий уровень дохода';
-} else if (budgetDay > 600 && budgetDay <= 1200) {
-  incomeLevel = 'У вас средний уровень дохода';
-} else if (budgetDay >= 0 && budgetDay <= 600) {
-  incomeLevel = 'К сожалению у вас уровень дохода ниже среднего';
+if (lang === 'ru') {
+  dayOfTheWeekIfElse = 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
 } else {
-  incomeLevel = 'Что-то пошло не так';
+  dayOfTheWeekIfElse = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
 }
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-console.log(addExpenses);
-console.log(`Дневной бюджет равен ${budgetDay} р.`);
-console.log(`Месячный бюджет равен ${budgetMonth} р.`);
-console.log(`Количество месяцев до цели: ${monthsToTarget}`);
-console.log(incomeLevel);
+switch (lang) {
+  case 'ru':
+    dayOfTheWeekSwitchCase = 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
+    break;
+  case 'en':
+    dayOfTheWeekSwitchCase = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
+}
+
+statusPerson = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
+
+console.log(dayOfTheWeekIfElse);
+console.log(dayOfTheWeekSwitchCase);
+console.log(dayOfTheWeekArray);
+console.log(statusPerson);
