fc8887f7c7b59ea303521bfd6859e01da9937a5d
Roman Baukin <rbaukin1984@gmail.com>
2020-11-24 22:35:51 +0400

    Исправил название переменных и подредактировал тело функции

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 'use strict';
 
-let a = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+let varText = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
 Minus, ipsam laboriosam quae dolores neque numquam ipsum doloribus 
 quam magni rerum maxime quos dicta esse earum eveniet. Culpa, libero at corrupti
 ducimus sed delectus possimus. Delectus veniam sunt, dicta adipisci molestiae, 
 ipsam cumque, expedita minus at itaque deserunt odit et in?`,
-  b = 123,
-  c = true;
+  varNumber = 123,
+  varBoolean = true;
 
 let stringProcessing = function (str) {
   if (typeof str !== 'string') {
     alert('Это не строка');
+  } else {
+    str = str.trim();
+    if (str.length > 30) {
+      str = str.substr(0, 30) + '...';
+    }
+    console.log(str);
   }
-  str = str.trim();
-  if (str.length > 30) {
-    str = str.substr(0, 30) + '...';
-  }
-  console.log(str);
 };
 
-stringProcessing(a);
+stringProcessing(varBoolean);
