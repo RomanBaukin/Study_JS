2f87fe93eac627cc11019e9e1ebfb161bdc13561
Roman Baukin <rbaukin1984@gmail.com>
2020-11-25 16:47:23 +0400

    Урок №5 усложненное задание

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
 'use strict';
 
-let varText = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-Minus, ipsam laboriosam quae dolores neque numquam ipsum doloribus 
-quam magni rerum maxime quos dicta esse earum eveniet. Culpa, libero at corrupti
-ducimus sed delectus possimus. Delectus veniam sunt, dicta adipisci molestiae, 
-ipsam cumque, expedita minus at itaque deserunt odit et in?`,
-  varNumber = 123,
-  varBoolean = true;
+// Первое задание
+let arr = [];
 
-let stringProcessing = function (str) {
-  if (typeof str !== 'string') {
-    alert('Это не строка');
-  } else {
-    str = str.trim();
-    if (str.length > 30) {
-      str = str.substr(0, 30) + '...';
+let outputNumberTwoAndFour = function () {
+  let indexArr = 0;
+  for (let i = 0; i < 7; i++) {
+    arr[i] = prompt('Введите многозначное число');
+  }
+
+  while (indexArr < 7) {
+    if (arr[indexArr][0] === '2' || arr[indexArr][0] === '4') {
+      console.log(arr[indexArr]);
     }
-    console.log(str);
+    indexArr++;
   }
 };
 
-stringProcessing(varBoolean);
+outputNumberTwoAndFour();
+
+// Второе задание
+let inputNumber = +prompt('Введите любое многозначное число', '100');
+
+let naturalNumbers = function (number) {
+  for (let i = 2; i <= number; i++) {
+    let flag = 1;
+    for (let j = 2; j <= i / 2 && flag === 1; j++) {
+      if (i % j === 0) {
+        flag = 0;
+      }
+    }
+    if (flag === 1) {
+      console.log(`${i} Делители этого числа: 1 и ${i}`);
+    }
+  }
+};
+
+naturalNumbers(inputNumber);
