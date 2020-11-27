31f7cc6bf661b3fbacedb8c5ae082e590da42731
Roman Baukin <rbaukin1984@gmail.com>
2020-11-27 15:34:12 +0400

    Урок №7 усложненное задание

--- js/script.js
+++ js/script.js
@@ -1,53 +1,13 @@
 'use strict';
 
-// Первое задание
-let arr = [];
+let myDate = new Date();
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-let isNumber = function (n) {
-  //проверяет, являются ли входящие данные числом
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+week[myDate.getDay() - 1] = week[myDate.getDay() - 1].bold();
 
-let outputNumberTwoAndFour = function () {
-  let indexArr = 0;
-  let multiDigitNumber;
-  for (let i = 0; i < 7; i++) {
-    do {
-      multiDigitNumber = prompt('Введите любое многозначное число');
-    } while (!isNumber(multiDigitNumber));
-
-    arr[i] = multiDigitNumber;
+for (let item of week) {
+  if (item === 'Суббота' || item === 'Воскресенье') {
+    item = item.italics();
   }
-
-  while (indexArr < 7) {
-    if (arr[indexArr][0] === '2' || arr[indexArr][0] === '4') {
-      console.log(arr[indexArr]);
-    }
-    indexArr++;
-  }
-};
-
-outputNumberTwoAndFour();
-
-// Второе задание
-let inputNumber;
-
-do {
-  inputNumber = prompt('Введите любое многозначное число', '100');
-} while (!isNumber(inputNumber));
-
-let naturalNumbers = function (number) {
-  for (let i = 2; i <= number; i++) {
-    let flag = 1;
-    for (let j = 2; j <= i / 2 && flag === 1; j++) {
-      if (i % j === 0) {
-        flag = 0;
-      }
-    }
-    if (flag === 1) {
-      console.log(`${i} Делители этого числа: 1 и ${i}`);
-    }
-  }
-};
-
-naturalNumbers(+inputNumber);
+  document.getElementById('week').innerHTML += item + '<br>';
+}
