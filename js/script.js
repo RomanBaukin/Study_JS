9ffeb5062cba636ea6d4f2a94ee0979881f38f1d
Roman Baukin <rbaukin1984@gmail.com>
2020-12-01 14:48:53 +0400

    Урок №9 усложненное задание

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 'use strict';
 
-let myDate = new Date(),
-  indexDay = myDate.getDay() - 1,
-  week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let today = Date.now();
 
-for (let key in week) {
-  if ((week[key] === 'Суббота' && +key === indexDay) || (week[key] === 'Воскресенье' && +key === indexDay)) {
-    week[key] = week[key].italics().bold();
-  } else if (week[key] === 'Суббота' || week[key] === 'Воскресенье') {
-    week[key] = week[key].italics();
-  } else if (+key === indexDay) {
-    week[key] = week[key].bold();
-  }
-  document.getElementById('week').innerHTML += week[key] + '<br>';
-}
+document.getElementById('today').innerHTML = today;
